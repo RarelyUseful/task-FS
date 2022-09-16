1829ca972233a617e569191b0457475e6a7a9372
Paweł Białobrodzki <bialobrodzki@gmail.com>
2022-09-16 22:06:10 +0200

    cleanup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { AppComponent } from './app.component';
 import { DisplayTasksObservableComponent } from './components/display-tasks-observable/display-tasks-observable.component';
 import { DisplayTasksComponent } from './components/display-tasks/display-tasks.component';
 
